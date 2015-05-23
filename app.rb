75a0a8ed339369cf67e612c1809d0f9dcb8a6d66
Dan Macon <dnmacon@gmail.com>
2015-05-23 11:11:55 -0400

    logging

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require 'sinatra'
+require 'logger'
+
+::Logger.class_eval { alias :write :'<<' }
+access_log = ::File.join(::File.dirname(::File.expand_path(__FILE__)),'log','access.log')
+access_logger = ::Logger.new(access_log)
+error_logger = ::File.new(::File.join(::File.dirname(::File.expand_path(__FILE__)),'log','error.log'),"a+")
+error_logger.sync = true
+
+configure do
+  use ::Rack::CommonLogger, access_logger
+end
 
 not_found do
   404
